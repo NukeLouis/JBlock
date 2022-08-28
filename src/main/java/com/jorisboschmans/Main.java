6f45442999aa1606125fc417d8f700edddd8aa5c
Joris Boschmans <jorisboschmans@gmail.com>
2022-08-28 16:57:47 +0200

    Better output thanks to Gson library

--- src/main/java/com/jorisboschmans/Main.java
+++ src/main/java/com/jorisboschmans/Main.java
@@ -1,21 +1,25 @@
 package com.jorisboschmans;
 
+import com.google.gson.GsonBuilder;
 import com.jorisboschmans.models.Block;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public final class Main {
+
+    private static final List<Block> chain = new ArrayList<>();
 
     private Main() {}
 
     public static void main(String[] args) {
 
-        Block block1 = new Block("Hello, I'm the first block", "0");
-        System.out.println("Hash for block 1: " + block1.hash);
+        chain.add(new Block("Hello, I'm the first block", "0"));
+        chain.add(new Block("Oi, I'm the second block", chain.get(chain.size() - 1).hash));
+        chain.add(new Block("Hi, I'm the third block", chain.get(chain.size() - 1).hash));
 
-        Block block2 = new Block("Oi, I'm the second block", block1.hash);
-        System.out.println("Hash for block 2: " + block2.hash);
-
-        Block block3 = new Block("Hi, I'm the third block", block2.hash);
-        System.out.println("Hash for block 3: " + block3.hash);
+        String chainJson = new GsonBuilder().setPrettyPrinting().create().toJson(chain);
+        System.out.println(chainJson);
 
     }
 
