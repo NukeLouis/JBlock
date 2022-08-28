5347be0f09dc5e1bce5c01e06000deaa7d742190
Joris Boschmans <jorisboschmans@gmail.com>
2022-08-28 17:20:05 +0200

    Added mining of blocks

--- src/main/java/com/jorisboschmans/Main.java
+++ src/main/java/com/jorisboschmans/Main.java
@@ -9,18 +9,56 @@ import java.util.List;
 public final class Main {
 
     private static final List<Block> chain = new ArrayList<>();
+    private static final int DIFFICULTY = 6;
 
     private Main() {}
 
     public static void main(String[] args) {
 
         chain.add(new Block("Hello, I'm the first block", "0"));
+        System.out.println("Trying to mine block 1...");
+        chain.get(0).mineBlock(DIFFICULTY);
         chain.add(new Block("Oi, I'm the second block", chain.get(chain.size() - 1).hash));
+        System.out.println("Trying to mine block 2...");
+        chain.get(1).mineBlock(DIFFICULTY);
         chain.add(new Block("Hi, I'm the third block", chain.get(chain.size() - 1).hash));
+        System.out.println("Trying to mine block 3...");
+        chain.get(2).mineBlock(DIFFICULTY);
+
+        if (!isChainValid())
+            throw new RuntimeException();
 
         String chainJson = new GsonBuilder().setPrettyPrinting().create().toJson(chain);
         System.out.println(chainJson);
 
+    }
+
+    private static boolean isChainValid() {
+        Block currentBlock;
+        Block previousBlock;
+        String hashTarget = new String(new char[DIFFICULTY]).replace('\0', '0');
+
+        for (int i = 1; i < chain.size(); i++) {
+            currentBlock = chain.get(i);
+            previousBlock = chain.get(i - 1);
+
+            if (!currentBlock.hash.equals(currentBlock.calculateHash())) {
+                System.out.println("Current hashes not equal");
+                return false;
+            }
+
+            if (!previousBlock.hash.equals(currentBlock.previousHash)) {
+                System.out.println("Previous hashes not equal");
+                return false;
+            }
+
+            if(!currentBlock.hash.substring(0, DIFFICULTY).equals(hashTarget)) {
+                System.out.println("This block hasn't been mined");
+                return false;
+            }
+        }
+
+        return true;
     }
 
 }
--- src/main/java/com/jorisboschmans/models/Block.java
+++ src/main/java/com/jorisboschmans/models/Block.java
@@ -10,6 +10,7 @@ public class Block {
     public String previousHash;
     private String data;
     private long timestampMilli;
+    private int nonce;
 
     public Block(String data, String previousHash) {
         this.data = data;
@@ -18,10 +19,20 @@ public class Block {
         this.hash = calculateHash();
     }
 
-    private String calculateHash() {
+    public String calculateHash() {
         return StringUtils.applySha256(
             previousHash
             + timestampMilli
+            + nonce
             + data);
+    }
+
+    public void mineBlock(int difficulty) {
+        String target = new String(new char[difficulty]).replace('\0', '0');
+        while (!hash.substring(0, difficulty).equals(target)) {
+            nonce++;
+            hash = calculateHash();
+        }
+        System.out.println("Block Mined!!! : " + hash);
     }
 }
